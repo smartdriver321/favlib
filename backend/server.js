ceb68e40849fdf46d507e022bb1ec9cdc7651089
smartdriver321 <tinggaldidisneyland@gmail.com>
2025-02-22 15:43:52 +0700

    Add localhost to origin

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,12 @@ const PORT = process.env.PORT || 8000
 const __dirname = path.resolve()
 
 // Middlewares
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL || 'http://localhost:5173',
+		credentials: true,
+	})
+)
 app.use(express.json({ limit: '20mb' }))
 app.use(cookieParser())
 
